5ce234f3286af1f13e5291d9ff0800172d335607
Haris Ahmad <119689990+harisahmad59@users.noreply.github.com>
2023-09-15 20:04:52 +0530

    Update script.js
    added statement outputs for bmi: underweight, normal and overweight

--- script.js
+++ script.js
@@ -5,3 +5,15 @@ function bmicalc(weight,height) {
 
 var bmi = bmicalc(65,1.8);
 console.log(bmi);
+
+if (bmi <= 18.5) {
+  document.write("your bmi is " + bmi + " so you are underweight.");
+}
+
+if (bmi > 18.5 && bmi <= 24.9) {
+  document.write("your bmi is " + bmi + " so you have a normal weight.");
+}
+
+if (bmi > 24.9) {
+  document.write("your bmi is " + bmi + " so you are overweight.");
+}
